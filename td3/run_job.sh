60e7356e7925ae0a1a7a331652e15abd25ad7ce2
Béla Umlauf <bela.umlauf@student.uni-tuebingen.de>
2025-02-07 19:03:17 +0100

    yep

---
+++ td3/run_job.sh
@@ -0,0 +1,42 @@
+#!/bin/bash --login
+#SBATCH -J td3               # Job name
+#SBATCH --ntasks=1                 # Number of tasks
+#SBATCH --cpus-per-task=1          # Number of CPU cores per task
+#SBATCH --nodes=1                  # Ensure that all cores are on the same machine with nodes=1
+#SBATCH --partition=a100-galvani   # Which partition will run your job
+#SBATCH --time=0-18:00             # Allowed runtime in D-HH:MM
+#SBATCH --gres=gpu:1               # (optional) Requesting type and number of GPUs
+#SBATCH --mem=50G                  # Total memory pool for all cores (see also --mem-per-cpu); exceeding this number will cause your job to fail.
+#SBATCH --output=logs/job-%j.out       # File to which STDOUT will be written - make sure this is not on $HOME
+#SBATCH --error=logs/myjob-%j.err        # File to which STDERR will be written - make sure this is not on $HOME
+#SBATCH --mail-type=FAIL            # Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --mail-user=bela.umlauf@student.uni-tuebingen.de   # Email to which notifications will be sent
+
+#module load anaconda
+conda activate rl_project
+
+# Ensure environment activation was successful
+if [ $? -ne 0 ]; then
+  echo "Failed to activate conda environment"
+  exit 1
+fi
+
+# Change to the `td3` directory where the script is located
+cd /mnt/qb/work/ludwig/lqb122/rl_project/td3
+
+# Run your code
+echo "-------- PYTHON OUTPUT ----------"
+python3 train_agent.py --results_folder ./results
+
+
+# Check if Python script ran successfully
+if [ $? -ne 0 ]; then
+  echo "Python script failed"
+  exit 1
+fi
+
+echo "---------------------------------"
+
+# Deactivate environment again
+conda deactivate
+
