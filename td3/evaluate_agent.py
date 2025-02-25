0e0568fb114df0fcb060b3edff6fc124d47e4bd7
Béla Umlauf <bela.umlauf@student.uni-tuebingen.de>
2025-02-25 21:15:18 +0100

    add arguments to eval_agent

--- td3/evaluate_agent.py
+++ td3/evaluate_agent.py
@@ -1,33 +1,43 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-
+import argparse
+import torch
 from td3.evaluate import evaluate
 from td3.td3_agent import TD3Agent
-import torch
 from hockey import hockey_env as h_env
 
-if __name__ == '__main__':
+
+def main(state_dict_path, max_episodes):
     print("### Start preparation ###")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Device: {device}")   
+    
     env = h_env.HockeyEnv(h_env.Mode.NORMAL)
     weak_opponent = h_env.BasicOpponent(weak=True)
     strong_opponent = h_env.BasicOpponent(weak=False) 
     opponents = [weak_opponent, strong_opponent]
+    
     agent = TD3Agent(env.observation_space, env.action_space, device, {})
-    state_dict_path="./results/competition/update_every_5_tau_0.005_lr_50_sp_30/td3_60000-t32-sNone.pth"
     agent.restore_state(torch.load(state_dict_path))
     agent.set_to_eval()
     assert agent.in_training == False
+    
     print("### Preparation done ###")
     print("### Start evaluation ###")
-    #play one game per opponent and record the results
+    
     for opponent in opponents:
-        max_episodes = 4
         wins, loses = evaluate(agent, env, opponent, max_episodes=max_episodes, max_timesteps=1000, render=True, agent_is_player_1=True)
         win_rate, lose_rate = sum(wins)/max_episodes, sum(loses)/max_episodes
         print(f"Winrate: {win_rate:.3f} Lossrate: {lose_rate:.3f}")
+    
     print("### Evaluation done ###")
     print("### End ###")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--state_dict_path', type=str, required=True, help='Path to the trained model state dict')
+    parser.add_argument('--max_episodes', type=int, default=4, help='Number of games to play against each opponent')
+    args = parser.parse_args()
+    
+    main(args.state_dict_path, args.max_episodes)
