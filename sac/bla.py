915c3e0bb7c936e5011bbfa24d589a0f2dee3723
lndwy <104760819+lndwy@users.noreply.github.com>
2025-02-26 12:14:34 +0100

    tidy up

--- sac/bla.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-model_path = r"C:\Users\regin\Documents\Winter24_25\rl_project\sac\logs\agents\10.pth"
-state = torch.load(model_path, map_location="cpu")
-
-print("📂 Gespeicherte Keys in state_dict:")
-print(state.keys())
